73b18d2ad0f3b0e5f0c75cb108f99112d4c41fc6
Vickymarz <okorojiebube2@gmail.com>
2026-02-16 17:48:58 +0100

    Add email notification

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -5,9 +5,9 @@ const app =
   getApps().length === 0
     ? initializeApp({
         credential: cert({
-          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-          clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         }),
       })
     : getApps()[0];
